f730eed3416e8cbf7ce438c2725af75c880a8b9e
azurekid <rogierdijkman@hotmail.com>
2026-08-31 16:58:25 +0200

    chore: changed case-sensitive field value

--- Solutions/Bitwarden/Data Connectors/AzureFunctionBitwarden/BitwardenTimerTrigger/functionApp/Modules/HelperFunctions.psm1
+++ Solutions/Bitwarden/Data Connectors/AzureFunctionBitwarden/BitwardenTimerTrigger/functionApp/Modules/HelperFunctions.psm1
@@ -320,7 +320,7 @@ function ConvertTo-EnrichedEventRecords {
 
         @{
             # Core event fields
-            TimeGenerated    = if ($ev.date) { $ev.date } else { $FallbackTimestamp }
+            EventTime    = if ($ev.date) { $ev.date } else { $FallbackTimestamp }
             eventType        = $ev.type
             itemId           = $ev.itemId
             collectionId     = $ev.collectionId
